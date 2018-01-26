9c75e19d57f0b9b2d42cca1caa90bc6f2793428a
神樹桜乃 <kodama@sakuno.moe>
2018-01-27 01:26:00 +0800

    Some extension methods

---
+++ src/Sakuno.UserInterface/DependencyObjectExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Sakuno.UserInterface
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class DependencyObjectExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsPropertyUnset(this DependencyObject obj, DependencyProperty dp) =>
+            obj.ReadLocalValue(dp) == DependencyProperty.UnsetValue;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void UpdateBindingTarget(this DependencyObject obj, DependencyProperty dp) =>
+            BindingOperations.GetBindingExpression(obj, dp)?.UpdateTarget();
+
+        public static IEnumerable<DependencyObject> EnumerateSelfAndAncestors(this DependencyObject obj)
+        {
+            while (obj != null)
+            {
+                yield return obj;
+
+                obj = VisualTreeHelper.GetParent(obj);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T GetParent<T>(this DependencyObject obj) where T : DependencyObject => VisualTreeHelper.GetParent(obj) as T;
+    }
+}
---
+++ src/Sakuno.UserInterface/TimelineExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Media.Animation;
+
+namespace Sakuno.UserInterface
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class TimelineExtensions
+    {
+        public static void OnCompleted(this Timeline timeline, Action continuation)
+        {
+            timeline.Completed += Handler;
+
+            void Handler(object sender, EventArgs e)
+            {
+                timeline.Completed -= Handler;
+
+                continuation();
+            }
+        }
+    }
+}
