9ed1975db360f183223ce6e5f8f6f807abb6eabd
神樹桜乃 <kodama@sakuno.moe>
2018-01-27 01:12:05 +0800

    High dpi support

---
+++ src/Sakuno.UserInterface/Dpi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace Sakuno.UserInterface
+{
+    public struct Dpi : IEquatable<Dpi>
+    {
+        public static readonly Dpi Default = new Dpi(96, 96);
+
+        public int X { get; }
+        public int Y { get; }
+
+        public double ScaleX { get; }
+        public double ScaleY { get; }
+
+        public Transform ScaleTransform => this == Default ? Transform.Identity : new ScaleTransform(ScaleX, ScaleY);
+
+        public Dpi(int x, int y)
+        {
+            X = x;
+            Y = y;
+
+            ScaleX = x / (double)Default.X;
+            ScaleY = y / (double)Default.Y;
+        }
+
+        public override bool Equals(object obj) => obj is Dpi dpi && Equals(dpi);
+        public bool Equals(Dpi other) => X == other.X && Y == other.Y;
+
+        public static bool operator ==(Dpi x, Dpi y) => x.Equals(y);
+        public static bool operator !=(Dpi x, Dpi y) => !x.Equals(y);
+
+        public override int GetHashCode() => (X * 401) ^ Y;
+    }
+}
---
+++ src/Sakuno.UserInterface/DpiUtil.cs
@@ -0,0 +1,27 @@
+﻿using Sakuno.SystemLayer;
+using System;
+
+namespace Sakuno.UserInterface
+{
+    public static class DpiUtil
+    {
+        public static Dpi SystemPrimary { get; }
+
+        static DpiUtil()
+        {
+            var hdc = NativeMethods.User32.GetDC(IntPtr.Zero);
+            if (hdc == IntPtr.Zero)
+            {
+                SystemPrimary = Dpi.Default;
+                return;
+            }
+
+            var x = NativeMethods.Gdi32.GetDeviceCaps(hdc, NativeConstants.DeviceCap.LOGPIXELSX);
+            var y = NativeMethods.Gdi32.GetDeviceCaps(hdc, NativeConstants.DeviceCap.LOGPIXELSY);
+
+            SystemPrimary = new Dpi(x, y);
+
+            NativeMethods.User32.ReleaseDC(IntPtr.Zero, hdc);
+        }
+    }
+}
