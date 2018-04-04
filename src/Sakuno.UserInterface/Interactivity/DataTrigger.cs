b1172be7e312b337cd66da03661ddb97506ffd37
神樹桜乃 <kodama@sakuno.moe>
2018-04-04 17:17:56 +0800

    Property change and data trigger

---
+++ src/Sakuno.UserInterface/Interactivity/DataTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace Sakuno.UserInterface.Interactivity
+{
+    public class DataTrigger : PropertyChangedTrigger
+    {
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(nameof(Value), typeof(object), typeof(DataTrigger),
+                new PropertyMetadata(OnValueChanged));
+
+        public object Value
+        {
+            get => GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) =>
+            ((DataTrigger)d).Evaulate(e);
+
+        public static readonly DependencyProperty ComparisonProperty =
+            DependencyProperty.Register(nameof(Comparison), typeof(ComparisonType), typeof(DataTrigger),
+                new PropertyMetadata(EnumUtil.GetBoxed(ComparisonType.Equal), OnComparisionChanged));
+
+        public ComparisonType Comparison
+        {
+            get => (ComparisonType)GetValue(ComparisonProperty);
+            set => SetValue(ComparisonProperty, EnumUtil.GetBoxed(value));
+        }
+        static void OnComparisionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) =>
+            ((DataTrigger)d).Evaulate(e);
+
+        protected override void Evaulate(DependencyPropertyChangedEventArgs e)
+        {
+            if (_associatedObject == null)
+                return;
+
+            if (!ComparisonUtil.Evaluate(Binding, Value, Comparison))
+                return;
+
+            base.Evaulate(e);
+        }
+    }
+}
---
+++ src/Sakuno.UserInterface/Interactivity/PropertyChangedTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace Sakuno.UserInterface.Interactivity
+{
+    public class PropertyChangedTrigger : Trigger
+    {
+        public static readonly DependencyProperty BindingProperty =
+            DependencyProperty.Register(nameof(Binding), typeof(object), typeof(PropertyChangedTrigger),
+                new PropertyMetadata(OnBindingChanged));
+
+        public object Binding
+        {
+            get => GetValue(BindingProperty);
+            set => SetValue(BindingProperty, value);
+        }
+
+        static void OnBindingChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) =>
+            ((PropertyChangedTrigger)d).OnBindingChanged(e);
+        void OnBindingChanged(DependencyPropertyChangedEventArgs e)
+        {
+            if (_associatedObject == null)
+                return;
+
+            Evaulate(e);
+        }
+
+        protected virtual void Evaulate(DependencyPropertyChangedEventArgs args)
+        {
+            foreach (var action in Actions)
+                action.UpdateBindingTargetsOfAllProperties();
+
+            InvokeActions(args);
+        }
+    }
+}
