fc62b672753c60b477d2e72adb21db28187c8981
神樹桜乃 <kodama@sakuno.moe>
2018-04-09 13:23:30 +0800

    Basic event trigger

---
+++ src/Sakuno.UserInterface/Interactivity/EventTrigger.cs
@@ -0,0 +1,60 @@
+﻿using Sakuno.Reflection;
+using System;
+using System.Windows;
+
+namespace Sakuno.UserInterface.Interactivity
+{
+    public class EventTrigger : EventTriggerBase
+    {
+        public static readonly DependencyProperty EventProperty =
+               DependencyProperty.Register(nameof(Event), typeof(string), typeof(EventTrigger),
+                   new FrameworkPropertyMetadata(OnEventChanged));
+
+        public string Event
+        {
+            get => (string)GetValue(EventProperty);
+            set => SetValue(EventProperty, value);
+        }
+
+        static void OnEventChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) =>
+            ((EventTrigger)d).OnEventChanged((string)e.OldValue, (string)e.NewValue);
+        void OnEventChanged(string oldValue, string newValue)
+        {
+            if (_associatedObject == null)
+                return;
+
+            var source = Source;
+            if (source == null)
+                return;
+
+            if (!oldValue.IsNullOrEmpty())
+                UnregisterEvent(source);
+
+            if (!newValue.IsNullOrEmpty())
+                RegisterEvent(source);
+        }
+
+        EventAccessor _eventAccessor;
+
+        Delegate _handler;
+
+        protected override void RegisterEvent(object source)
+        {
+            var type = source.GetType();
+            var eventInfo = type.GetEvent(Event) ?? throw new InvalidOperationException($"Event \"{Event}\" does not exist on type \"{type.FullName}\".");
+
+            _eventAccessor = new EventAccessor(eventInfo);
+
+            _handler = Delegate.CreateDelegate(_eventAccessor.Event.EventHandlerType, this, _handlerMethod);
+            _eventAccessor.AddHandler(source, _handler);
+        }
+        protected override void UnregisterEvent(object source)
+        {
+            if (_eventAccessor == null)
+                return;
+
+            _eventAccessor.RemoveHandler(source, _handler);
+            _handler = null;
+        }
+    }
+}
---
+++ src/Sakuno.UserInterface/Interactivity/EventTriggerBase.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace Sakuno.UserInterface.Interactivity
+{
+    public abstract class EventTriggerBase : Trigger
+    {
+        public static readonly DependencyProperty SourceObjectProperty =
+            DependencyProperty.Register(nameof(SourceObject), typeof(object), typeof(EventTriggerBase),
+                new PropertyMetadata(OnSourceObjectChanged));
+
+        [TypeConverter(typeof(NameReferenceConverter))]
+        public object SourceObject
+        {
+            get => GetValue(SourceObjectProperty);
+            set => SetValue(SourceObjectProperty, value);
+        }
+
+        static void OnSourceObjectChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) =>
+            ((EventTriggerBase)d).OnSourceObjectChanged(e.OldValue, e.NewValue);
+        void OnSourceObjectChanged(object oldValue, object newValue)
+        {
+            if (newValue == null)
+            {
+                OnSourceChanged(oldValue, _associatedObject);
+                return;
+            }
+
+            VerifySourceType(newValue);
+
+            if (_associatedObject == null)
+                return;
+
+            OnSourceChanged(oldValue, newValue);
+        }
+
+        private protected static readonly MethodInfo _handlerMethod;
+
+        Type _sourceTypeConstraint;
+
+        protected object Source => SourceObject ?? AssociatedObject;
+
+        static EventTriggerBase()
+        {
+            _handlerMethod = typeof(EventTriggerBase).GetMethod(nameof(OnEventRaisedCore), BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+        protected EventTriggerBase() : this(null) { }
+        private protected EventTriggerBase(Type sourceTypeConstraint)
+        {
+            _sourceTypeConstraint = sourceTypeConstraint;
+        }
+
+        protected override void OnAttached()
+        {
+            VerifySourceType(_associatedObject);
+            OnSourceChanged(null, Source);
+        }
+        protected override void OnDetaching() => OnSourceChanged(Source, null);
+
+        void VerifySourceType(object target)
+        {
+            if (_sourceTypeConstraint != null && !_sourceTypeConstraint.IsAssignableFrom(target.GetType()))
+                throw new InvalidOperationException($"The type of source should be \"{_sourceTypeConstraint.FullName}\" or its derived type.");
+        }
+        void OnSourceChanged(object oldValue, object newValue)
+        {
+            if (oldValue != null)
+                UnregisterEvent(oldValue);
+
+            if (newValue != null)
+                RegisterEvent(newValue);
+        }
+
+        protected abstract void RegisterEvent(object source);
+        protected abstract void UnregisterEvent(object source);
+
+        private protected void OnEventRaisedCore(object sender, EventArgs e) => OnEventRaised(e);
+        protected virtual void OnEventRaised(EventArgs e) => InvokeActions(e);
+    }
+
+    public abstract class EventTriggerBase<T> : EventTriggerBase where T : class
+    {
+        protected EventTriggerBase() : base(typeof(T)) { }
+
+        protected sealed override void RegisterEvent(object source) => RegisterEvent(source as T);
+        protected sealed override void UnregisterEvent(object source) => UnregisterEvent(source as T);
+
+        protected abstract void RegisterEvent(T source);
+        protected abstract void UnregisterEvent(T source);
+    }
+}
