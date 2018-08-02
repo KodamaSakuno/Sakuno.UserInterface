f29af82b50e436cbf3b0faf75560ffb8ab60deff
神樹桜乃 <kodama@sakuno.moe>
2018-08-02 15:35:08 +0800

    Use ThemedApp instead

--- samples/Sakuno.UserInterface.Demo/App.xaml.cs
+++ samples/Sakuno.UserInterface.Demo/App.xaml.cs
@@ -4,11 +4,5 @@ namespace Sakuno.UserInterface.Demo
 {
     partial class App
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            ThemeManager.Instance.Initialize(this, Themes.Light, Accents.Blue);
-
-            base.OnStartup(e);
-        }
     }
 }
---
+++ src/Sakuno.UserInterface/ThemedApp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Sakuno.UserInterface
+{
+    public class ThemedApp : Application, INotifyPropertyChanged
+    {
+        ResourceDictionary _root = new ResourceDictionary();
+
+        Theme _theme;
+        public Theme Theme
+        {
+            get => _theme;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+
+                if (_theme == value)
+                    return;
+
+                OverwriteItems(value.ResourceDictionary, Theme.EnumerateRequiredKeys());
+
+                _theme = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        Accent _accent;
+        public Accent Accent
+        {
+            get => _accent;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+
+                if (_accent == value)
+                    return;
+
+                OverwriteItems(value.ResourceDictionary, Accent.EnumerateRequiredKeys());
+
+                _accent = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected ThemedApp()
+        {
+            Theme = Themes.Light;
+            Accent = Accents.Blue;
+
+            Resources.MergedDictionaries.Add(_root);
+        }
+
+        void OverwriteItems(ResourceDictionary dictionary, string[] keys)
+        {
+            foreach (var key in keys)
+                if (!dictionary.Contains(key))
+                    throw new KeyNotFoundException(key);
+
+            foreach (var key in keys)
+                _root[key] = dictionary[key];
+        }
+
+        void NotifyPropertyChanged([CallerMemberName] string property = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+    }
+}
