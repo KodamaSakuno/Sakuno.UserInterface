9170d01dda382802d03f996f92414e76e9ade8f9
神樹桜乃 <kodama@sakuno.moe>
2018-08-31 15:42:06 +0800

    Correct the colors of close button

---
+++ samples/Sakuno.UserInterface.Demo/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sakuno.UserInterface.Demo
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        ThemedApp App => Application.Current as ThemedApp;
+
+        Theme _theme;
+        public Theme Theme
+        {
+            get => _theme;
+            set
+            {
+                if (_theme != value)
+                {
+                    App.Theme = value;
+                    _theme = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            _theme = App.Theme;
+        }
+
+        void NotifyPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
