2b62162aabc69b3cd1a6ab749ef039082aa3c514
神樹桜乃 <kodama@sakuno.moe>
2019-01-08 17:11:17 +0800

    Add NullToVisibilityConverter

---
+++ src/Sakuno.UserInterface/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Sakuno.UserInterface.Converters
+{
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public sealed class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            EnumUtil.GetBoxed(value == null ? Visibility.Visible : Visibility.Collapsed);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+    }
+}
