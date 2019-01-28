cdd862fe3427d2c0254994ba0d064645095da01f
神樹桜乃 <kodama@sakuno.moe>
2019-01-28 17:50:30 +0800

    A markup extension which provides the root object of XAML document

---
+++ src/Sakuno.UserInterface/Markup/DocumentRootExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Xaml;
+
+namespace Sakuno.UserInterface.Markup
+{
+    public sealed class DocumentRootExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var rootObjectProvider = serviceProvider.GetService<IRootObjectProvider>() ??
+                throw new InvalidOperationException("There is no RootObjectProvider service in the service provider.");
+
+            return rootObjectProvider.RootObject;
+        }
+    }
+}
