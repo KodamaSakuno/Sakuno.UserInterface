cd09bd9fdcfad94be17120706c4feba79b680927
神樹桜乃 <kodama@sakuno.moe>
2019-01-29 21:18:15 +0800

    Add a value converter to inverse boolean value

---
+++ src/Sakuno.UserInterface/Converters/InvertBooleanConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Sakuno.UserInterface.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public sealed class InvertBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => ConvertCore(value);
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => ConvertCore(value);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        object ConvertCore(object value)
+        {
+            if (value is bool boolean)
+                return BooleanUtil.GetBoxed(!boolean);
+
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
