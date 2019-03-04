0db2a07bf6d5e95c5a9bb658c70d7c6f17bf3fce
神樹桜乃 <kodama@sakuno.moe>
2019-03-04 20:39:58 +0800

    Add is null or not value converter

---
+++ src/Sakuno.UserInterface/Converters/IsNullConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Sakuno.UserInterface.Converters
+{
+    [ValueConversion(typeof(object), typeof(bool))]
+    public sealed class IsNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => BooleanUtil.GetBoxed(value is null);
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+    }
+}
