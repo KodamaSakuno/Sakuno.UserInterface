98d5fd8c42f3fdee1637c871bba9a619346d9735
神樹桜乃 <kodama@sakuno.moe>
2019-04-02 21:33:33 +0800

    Add DropDownButton

---
+++ src/Sakuno.UserInterface/Controls/DropDownButton.cs
@@ -0,0 +1,26 @@
+﻿using Sakuno.UserInterface.Controls.Primitives;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Sakuno.UserInterface.Controls
+{
+    [TemplatePart(Name = "PART_ToggleButton", Type = typeof(ToggleButton))]
+    [TemplatePart(Name = "PART_Popup", Type = typeof(Popup))]
+    public class DropDownButton : ButtonWithPopup
+    {
+        public static readonly DependencyProperty ShowDropDownMarkerProperty =
+            DependencyProperty.Register(nameof(ShowDropDownMarker), typeof(bool), typeof(DropDownButton),
+                new FrameworkPropertyMetadata(BoxedConstants.Boolean.True));
+
+        public bool ShowDropDownMarker
+        {
+            get => (bool)GetValue(ShowDropDownMarkerProperty);
+            set => SetValue(ShowDropDownMarkerProperty, BooleanUtil.GetBoxed(value));
+        }
+
+        static DropDownButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DropDownButton), new FrameworkPropertyMetadata(typeof(DropDownButton)));
+        }
+    }
+}
---
+++ src/Sakuno.UserInterface/Controls/Primitives/ButtonWithPopup.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace Sakuno.UserInterface.Controls.Primitives
+{
+    [TemplatePart(Name = "PART_ToggleButton", Type = typeof(ToggleButton))]
+    [TemplatePart(Name = "PART_Popup", Type = typeof(Popup))]
+    public abstract class ButtonWithPopup : HeaderedContentControl
+    {
+        public static readonly DependencyProperty PopupVerticalOffsetProperty =
+            DependencyProperty.Register(nameof(PopupVerticalOffset), typeof(double), typeof(ButtonWithPopup),
+                new FrameworkPropertyMetadata(BoxedConstants.Double.Zero));
+
+        public double PopupVerticalOffset
+        {
+            get => (double)GetValue(PopupVerticalOffsetProperty);
+            set => SetValue(PopupVerticalOffsetProperty, value);
+        }
+
+        public static readonly DependencyProperty PopupHorizontalOffsetProperty =
+            DependencyProperty.Register(nameof(PopupHorizontalOffset), typeof(double), typeof(ButtonWithPopup),
+                new FrameworkPropertyMetadata(BoxedConstants.Double.Zero));
+        public double PopupHorizontalOffset
+        {
+            get => (double)GetValue(PopupHorizontalOffsetProperty);
+            set => SetValue(PopupHorizontalOffsetProperty, value);
+        }
+
+        public static readonly DependencyProperty IsPopupOpenedProperty =
+            DependencyProperty.Register(nameof(IsPopupOpened), typeof(bool), typeof(ButtonWithPopup),
+                new FrameworkPropertyMetadata(BoxedConstants.Boolean.False, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public bool IsPopupOpened
+        {
+            get => (bool)GetValue(IsPopupOpenedProperty);
+            set => SetValue(IsPopupOpenedProperty, BooleanUtil.GetBoxed(value));
+        }
+    }
+}
