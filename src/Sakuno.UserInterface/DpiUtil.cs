42a830feab7a8056894603d6e4cf8f52b10cb0fa
神樹桜乃 <kodama@sakuno.moe>
2019-04-03 19:24:04 +0800

    Add methods to round layout value by DPI

--- src/Sakuno.UserInterface/DpiUtil.cs
+++ src/Sakuno.UserInterface/DpiUtil.cs
@@ -1,5 +1,6 @@
 ﻿using Sakuno.SystemLayer;
 using System;
+using System.Windows;
 using System.Windows.Media;
 
 namespace Sakuno.UserInterface
@@ -31,5 +32,30 @@ namespace Sakuno.UserInterface
 
             return new Dpi(result.DpiScaleX, result.DpiScaleY);
         }
+
+        public static double RoundLayoutValue(double value, double scale)
+        {
+            if (DoubleUtil.IsCloseToOne(scale))
+                return Math.Round(value);
+
+            const long EncodingOfDoubleMaxValue = 0x7FEFFFFFFFFFFFFFL;
+
+            var result = Math.Round(value * scale) / scale;
+            if ((BitConverter.DoubleToInt64Bits(result) & long.MaxValue) < EncodingOfDoubleMaxValue)
+                return result;
+
+            return value;
+        }
+
+        public static Size RoundLayoutValue(in Size size, in DpiScale dpiScale) =>
+            new Size(RoundLayoutValue(size.Width, dpiScale.DpiScaleX), RoundLayoutValue(size.Height, dpiScale.DpiScaleY));
+        public static Point RoundLayoutValue(in Point point, in DpiScale dpiScale) =>
+            new Point(RoundLayoutValue(point.X, dpiScale.DpiScaleX), RoundLayoutValue(point.Y, dpiScale.DpiScaleY));
+        public static Thickness RoundLayoutValue(in Thickness thickness, in DpiScale dpiScale) =>
+            new Thickness(
+                RoundLayoutValue(thickness.Left, dpiScale.DpiScaleX),
+                RoundLayoutValue(thickness.Top, dpiScale.DpiScaleY),
+                RoundLayoutValue(thickness.Right, dpiScale.DpiScaleX),
+                RoundLayoutValue(thickness.Bottom, dpiScale.DpiScaleY));
     }
 }
