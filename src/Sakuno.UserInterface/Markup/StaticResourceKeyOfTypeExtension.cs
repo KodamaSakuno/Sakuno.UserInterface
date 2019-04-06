de4a8f603827932db7dc426cfff3739096630354
神樹桜乃 <kodama@sakuno.moe>
2019-04-06 21:18:50 +0800

    A markup extension to simply a dull pattern like {StaticResource {x:Type}}

---
+++ src/Sakuno.UserInterface/Markup/StaticResourceKeyOfTypeExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace Sakuno.UserInterface.Markup
+{
+    public sealed class StaticResourceKeyOfTypeExtension : MarkupExtension
+    {
+        public Type Type { get; }
+
+        public StaticResourceKeyOfTypeExtension(Type type)
+        {
+            Type = type ?? throw new ArgumentNullException(nameof(type));
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) =>
+            new StaticResourceExtension(Type).ProvideValue(serviceProvider);
+    }
+}
